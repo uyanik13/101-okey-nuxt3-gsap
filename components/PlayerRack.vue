a07732130b85bd9a09394e91bf594e4bfed2aaa5
Oğur UYANIK <ogur.uyanik@pfizer.com>
2023-04-22 22:32:29 +0300

    used array base tiles

--- components/PlayerRack.vue
+++ components/PlayerRack.vue
@@ -19,15 +19,13 @@
           id="tiles"
           class="flex flex-wrap tiles rack-bg border-t-2 border-b-2 border-gray-200 h-42 tiles"
         >
-          <div
-            ref="tileContainers"
-            v-for="(tile, index) in section1Tiles"
+          <Tile
+            v-for="(tile, index) in playerTiles"
             :key="index"
-            class="tile-container"
-            :data-index="index"
-          >
-            <Tile :number="tile.number" :color="tile.color" />
-          </div>
+            :dataIndex="index"
+            :number="tile.number"
+            :color="tile.color"
+          />
         </div>
 
         <div
@@ -54,22 +52,19 @@
 import { ref, defineProps, onMounted } from "vue";
 import tiles from "@/assets/fixtures/tiles.json";
 import Tile from "@/components/Tile.vue";
-import { useOverlap } from '@/composables/useOverlap.js';
+import { useOverlap } from "@/composables/useOverlap.js";
+import consolaGlobalInstance from "consola";
+import { remove } from "@vue/shared";
 const { $gsap: gsap, $Draggable: Draggable } = useNuxtApp();
 const { getOverlapRatio } = useOverlap();
 
-const section1Tiles = ref([
-  ...tiles,
-  ...Array(11).fill({ number: null, color: null }),
-]);
+const playerTiles = ref([...tiles]);
 
-const tileContainers = ref([]);
 const tilesRef = ref(null);
-const highlightedTarget = ref(null);
+const tileDivs = ref([]);
 
 const onDrag = (tile, event) => {
-  addCustoms(tile);
-  const target = findTargetContainer(tile);
+  const target = findTargetTile(tile);
   if (target) {
     gsap.killTweensOf(tile);
   }
@@ -83,117 +78,86 @@ const onRelease = (tile) => {
 };
 
 const onDragEnd = (tile, event) => {
-  const target = findTargetContainer(tile);
-  changePosIfHasTarget(tile, target);
-  if (target) {
+  const target = findTargetTile(tile);
+  if (target.index) {
     changePosition(tile, target);
     gsap.set(tile, {
       clearProps: "transform",
       zIndex: 1000,
     });
-  }
-  removeCustoms(tile);
-  reorderTiles();
-};
-
-const reorderTiles = () => {
-  tileContainers.value.forEach((container) => {
-    const tiles = Array.from(container.children).sort((a, b) => {
-      const aRect = a.getBoundingClientRect();
-      const bRect = b.getBoundingClientRect();
-      return aRect.left - bRect.left;
-    });
-    tiles.forEach((tile) => container.appendChild(tile));
-  });
-};
-
-const addCustoms = (tile) => {
-  const newTarget = findTargetContainer(tile);
-  if (newTarget !== highlightedTarget.value) {
-    if (highlightedTarget.value) {
-      highlightedTarget.value.classList.remove("highlight");
-    }
-    highlightedTarget.value = newTarget;
-    if (highlightedTarget.value) {
-      highlightedTarget.value.classList.add("highlight");
-    }
-  }
-  tile.classList.add("dragging");
-};
-
-const removeCustoms = (tile) => {
-  const target = findTargetContainer(tile);
-  target.classList.remove("highlight");
-  tile.classList.remove("dragging");
-  highlightedTarget.value.classList.remove("highlight");
-};
-
-const findTargetContainer = (tile) => {
-  let target = null;
-  let maxOverlapRatio = 0;
-
-  tileContainers.value.forEach((container) => {
-    const overlapRatio = getOverlapRatio(tile, container);
-    if (overlapRatio > maxOverlapRatio) {
-      target = container;
-      maxOverlapRatio = overlapRatio;
-    }
-  });
-
-  if (maxOverlapRatio > 0.1) {
-    return target;
   } else {
-    return tile.parentElement;
+    gsap.set(tile, {
+      clearProps: "transform",
+      zIndex: 1000,
+    });
   }
 };
 
-
-const changePosIfHasTarget = (tile1, target) => {
-
-  if (target) {
-    const tilesInTarget = Array.from(target.children);
-    if (tilesInTarget.length) {
-      const tile2 = tilesInTarget[tilesInTarget.length - 1];
-      let tile1Index = tile1.dataset.index;
-      let tile2Index = tile2.dataset.index;
-      tile1.dataset.moved = "true";
-      tile2.dataset.moved = "true";
-      tile1Index > tile2Index ? tile2.after(tile1) : tile1.after(tile2);
-
-      console.log(tile2)
-      // Move the rest of the tiles
-      for (let i = 1; i < tilesInTarget.length - 1; i++) {
-        const tile = tilesInTarget[i];
-      
-        tile.dataset.moved = "true";
-        tile1Index > tile2Index ? tile2.after(tile) : tile.before(tile2);
+const findTargetTile = (movedTile) => {
+  let target = {
+    item: false,
+    index: null,
+  };
+  let foundTargetInHitTest = false;
+  tileDivs.value.forEach((tile) => {
+    if (tile !== movedTile) {
+      const hitTestResult = Draggable.hitTest(movedTile, tile, "50%");
+      if (hitTestResult) {
+        target.item = true;
+        target.index = tile.dataset.index;
+        foundTargetInHitTest = true;
       }
     }
+  });
+
+  if (!foundTargetInHitTest) {
+    let minDistance = Infinity;
+    tileDivs.value.forEach((targetTile) => {
+      if (targetTile !== movedTile) {
+        const distance = calculateDistance(movedTile, targetTile);
+        if (distance < minDistance) {
+          target.item = true;
+          target.index = targetTile.dataset.index;
+          minDistance = distance;
+        }
+      }
+    });
   }
+
+  return target;
+};
+
+const calculateDistance = (element1, element2) => {
+  const dx = element1.offsetLeft - element2.offsetLeft;
+  const dy = element1.offsetTop - element2.offsetTop;
+  return Math.sqrt(dx * dx + dy * dy);
 };
 
 
-const changePosition = (tile, zone) => {
-  while (zone.firstChild) {
-    zone.removeChild(zone.firstChild);
-  }
-  zone.appendChild(tile);
+const changePosition = (tile, target) => {
+  console.log(target);
+  const tileIndex = parseInt(tile.dataset.index);
+  if (target.item) {
+    const movedElement = playerTiles.value.splice(tileIndex, 1)[0];
+    playerTiles.value.splice(target.index, 0, movedElement);
     gsap.to(tile, {
-      keyframes: {
-        x: [1, -1],
-      },
-      onComplete: function () {
-        tile.style.transform = "";
-        tile.style.rotate = "";
-        tile.dataset.moved = "false";
+      duration: 1,
+      x: 1,
+      y: 0,
+      onComplete: () => {
+        gsap.set(tile, {
+          x: 0,
+          y: 0,
+          zIndex: 1,
+        });
       },
     });
-
+  }
 };
 
 onMounted(() => {
-  const tiles = tilesRef.value.querySelectorAll(".tile");
-  tiles.forEach((tile) => {
+  tileDivs.value = tilesRef.value.querySelectorAll(".tile");
+  tileDivs.value.forEach((tile) => {
     Draggable.create(tile, {
       bounds: ".game-area",
       type: "x,y",
@@ -205,7 +169,6 @@ onMounted(() => {
       onRelease: onRelease.bind(this, tile),
     });
   });
-  tileContainers.value = Array.from(tileContainers.value);
 });
 </script>
 
--- components/Tile.vue
+++ components/Tile.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    v-if="number"
-    class="tile tile-bg border-1 border-gray-500 rounded-lg w-14 h-20 flex flex-col items-center justify-center"
+    :class="number ? 'tile tile-style' :'tile'"
     :style="{ color: colorCode }"
+    :data-index="dataIndex"
   >
     <div class="number font-merri text-4xl">{{ number }}</div>
     <span class="w-3 h-3 bg-gray-400 shadow-inner rounded-full mt-2"></span>
@@ -14,6 +14,7 @@ import { defineProps } from "vue";
 const props = defineProps({
   number: Number,
   color: String,
+  dataIndex:Number
 });
 
 const colorCode = computed (()=> {
